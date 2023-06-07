54c0fbc9307c1dae8ae3322c121bbb99a532e8a9
vaishusonvane <110886113+vaishusonvane@users.noreply.github.com>
2023-06-07 10:58:48 +0530

    first commit

--- index.ts
+++ index.ts
@@ -1,26 +1,4 @@
-let a=5;
-console.log(a);
-
-const names: string[] = [];
-names.push("hii");
-
-function sum(a:number,b:number) {
-    return (a+b)
+function add(a,b){
+    return a+b;
 }
-
-let address : [number, string, number];
-address = [28, "vaishu", 7]
-
-interface person{
-    name : string;
-    id?: number;
-    age: number;
-}
-
-let p: person;
-p = {name: "vaishu", age: 7}
-
-
-let sales: number;
-let cousre: string;
-let sale: bigint:
+console.log(add(10,90));
